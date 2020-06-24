978f81ef18b530c99279e5f929f1ce200832e966
NateTheDev1 <>
2020-06-23 22:52:24 -0400

    node_modules

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+
+// MONGOOSE SETUP HERE
+// mongoose.connect(
+//     process.env.DB_CONNECTION,
+//     { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false },
+//     () => {
+//       console.log("Db Connected");
+//     }
+//   );
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the student view API')
+})  
+
+// const port = process.env.PORT || 5000
+app.listen(5000, () => {
+    console.log('Listening on ' + 5000)
+})
