ad275b0e2a4de0ac5036894a21b470b2daac49e9
NateTheDev1 <nthnlrichards@gmail.com>
2020-06-24 11:28:54 -0400

    Delete a student done

--- routes/leaderRoutes.js
+++ routes/leaderRoutes.js
@@ -14,7 +14,7 @@ router.post("/student", async (req, res) => {
     { new: true },
     (err, doc) => {
       if (err) {
-        res.status.send("Error adding student.");
+        res.status(400).send("Error adding student.");
       }
       return res.json(doc);
     }
@@ -36,7 +36,39 @@ router.put("/student", async (req, res) => {
       return s;
     }
   });
-  console.log(newStudents);
+
+  Leader.findOneAndUpdate(
+    { _id: req.body.leaderId },
+    { $set: { students: newStudents } },
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        res.status(400).send("Error updating student.");
+      }
+      return res.json(doc);
+    }
+  );
+});
+
+router.delete("/student", async (req, res) => {
+  const leader = await Leader.findOne({ _id: req.body.leaderId });
+  const students = leader.students;
+
+  const newStudents = students.filter((s) => {
+    return s.studentId !== req.body.studentId;
+  });
+
+  Leader.findOneAndUpdate(
+    { _id: req.body.leaderId },
+    { $set: { students: newStudents } },
+    { new: true },
+    (err, doc) => {
+      if (err) {
+        res.status(400).send("Error deleting student.");
+      }
+      return res.json(doc);
+    }
+  );
 });
 
 module.exports = router;
